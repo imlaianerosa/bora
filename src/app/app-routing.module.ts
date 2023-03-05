a9d026b4908737a1c5f1252b228b35deed7f02c8
System Administrator <root@Laianes-MacBook-Pro.local>
2023-03-05 14:32:41 -0300

    initial-load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitialLoadComponent } from './initial-load/initial-load.component';
 
 const routes: Routes = [];
 
@@ -7,4 +8,11 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  Routes = [
+    {
+      path: '/load',
+      component: InitialLoadComponent
+    }
+  ]
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitialLoadComponent } from './initial-load/initial-load.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialLoadComponent
   ],
   imports: [
     BrowserModule,
